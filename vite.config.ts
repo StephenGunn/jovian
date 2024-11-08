1f30773ef8c91b2ef8354165d6898ef23e85762d
Stephen Gunn <stephengunn@gmail.com>
2024-11-08 15:58:04 -0600

    try

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+  plugins: [sveltekit()],
   server: {
     port: 42069
   },
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  optimizeDeps: {
+    exclude: ["@resvg/resvg-js"]
+  },
+  build: {
+    rollupOptions: {
+      external: ["@resvg/resvg-js"]
+    }
+  },
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"]
+  }
 });
